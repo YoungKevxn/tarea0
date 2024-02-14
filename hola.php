0e94d7ff8b0ee908d247481b77ceb6a59396dba1
YoungKevxn <159972938+YoungKevxn@users.noreply.github.com>
2024-02-15 00:56:19 +0100

    Add files via upload

---
+++ hola.php
@@ -0,0 +1,43 @@
+<html>
+    <head></head>
+    <body>
+        <h1>12655. C09</h1>
+        <table border="1">
+    <tr>
+        <td>Puntos</td>
+        <td>14.96</td>
+        <td>Límite de memoria</td>
+        <td>32 MiB</td>
+    <tr>
+        <td>Límite de tiempo (caso)</td>
+        <td>1s</td>
+        <td>Límite de tiempo (total)</td>
+        <td>1m0s</td>
+    <tr>
+        <td>Tamaño límite de entrada (bytes)</td>
+        <td>10 KiB</td>
+    </tr>
+        </table>
+        <h2>Descripcion</h2>
+        Escribe un programa que solicite las calificaciones de Matemáticas, Español y Cocina y nos de su promedio.
+        <h2>Entrada</h2>
+        Tres números reales
+        <h2>Salida</h2>
+        Un número real representado el promedio de los tres números
+        <h2>Ejemplo</h2>
+
+        <h4>Entrada</h4>
+        10 9.5 9.0
+        <h4>Salida</h4>
+        9.5
+
+        <?php
+        $a=10;
+        $b=9.5;
+        $c=9.0;
+        $op=$a+$b+$c;
+        $r=$op/3;
+        echo "Resultado", $r;
+        ?>
+    </body>
+</html>
